84e6488dd8e5d14cdd4326bad4e8df4fabe72dcb
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-03-02 21:33:25 +0700

    Add modelmapper bean

--- src/main/java/ru/cft/licenseservice/LicenseServiceConfiguration.java
+++ src/main/java/ru/cft/licenseservice/LicenseServiceConfiguration.java
@@ -1,9 +1,15 @@
 package ru.cft.licenseservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 public class LicenseServiceConfiguration {
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
