6b8fcf68db6b88c993e3853eb9f4070f63d900bb
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-03-07 14:57:23 +0700

    Add license serialization service

---
+++ src/main/java/ru/cft/licenseservice/service/LicenseSerializationService.java
@@ -0,0 +1,27 @@
+package ru.cft.licenseservice.service;
+
+import com.esotericsoftware.kryo.kryo5.Kryo;
+import com.esotericsoftware.kryo.kryo5.io.Input;
+import com.esotericsoftware.kryo.kryo5.io.Output;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.cft.licenseservice.dto.LicenseFileDto;
+
+@Service
+@RequiredArgsConstructor
+public class LicenseSerializationService {
+	private final Kryo kryo;
+
+	public byte[] serialize(LicenseFileDto license) {
+		try (Output output = new Output()) {
+			kryo.writeObject(output, license);
+			return output.toBytes();
+		}
+	}
+
+	public LicenseFileDto deserialize(byte[] file) {
+		try (Input input = new Input(file)) {
+			return kryo.readObject(input, LicenseFileDto.class);
+		}
+	}
+}
