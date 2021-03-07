c50471dee2a16600bcb218d79f800b55de66606b
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-03-07 14:50:49 +0700

    Add license entity

--- src/main/java/ru/cft/licenseservice/entity/License.java
+++ src/main/java/ru/cft/licenseservice/entity/License.java
@@ -1,19 +1,23 @@
 package ru.cft.licenseservice.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.security.KeyPair;
+import java.time.Instant;
 
 @Entity
+@Table(name = "licenses")
+@Data
 public class License {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    @Override
-    public String toString() {
-        return "License with id: " + id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private Instant startDate;
+    private Instant expirationDate;
+    private KeyPair keyPair;
 }
