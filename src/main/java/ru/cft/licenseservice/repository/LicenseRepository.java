dac6feba4530a25fe81d7d13aa210d712b59bfd3
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-03-12 16:43:32 +0700

    Add expiring licenses update scheduling

--- src/main/java/ru/cft/licenseservice/repository/LicenseRepository.java
+++ src/main/java/ru/cft/licenseservice/repository/LicenseRepository.java
@@ -3,4 +3,8 @@ package ru.cft.licenseservice.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.cft.licenseservice.entity.License;
 
-public interface LicenseRepository extends CrudRepository<License, Long> {}
+import java.time.Instant;
+
+public interface LicenseRepository extends CrudRepository<License, Long> {
+	Iterable<License> findAllByExpirationDateAfterAndExpirationDateBefore(Instant min, Instant max);
+}
--- src/main/java/ru/cft/licenseservice/service/ScheduledTasks.java
+++ src/main/java/ru/cft/licenseservice/service/ScheduledTasks.java
@@ -1,13 +1,35 @@
 package ru.cft.licenseservice.service;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.cft.licenseservice.entity.ExpiringLicense;
+import ru.cft.licenseservice.entity.License;
+import ru.cft.licenseservice.repository.ExpiringLicenseRepository;
+import ru.cft.licenseservice.repository.LicenseRepository;
 
-@EnableScheduling
+import java.time.Instant;
+import java.time.Period;
+
+@Component
+@RequiredArgsConstructor
 public class ScheduledTasks {
+    private final ExpiringLicenseRepository expiringRepository;
+    private final LicenseRepository licenseRepository;
 
-    @Scheduled(fixedRate = 86400)
-    public void reportCurrentTime() {
-
+    @Scheduled(fixedRateString = "${service.expiring-update.delay-in-milliseconds}")
+    public void updateExpired() {
+        System.out.println("test");
+        Instant now = Instant.now();
+        Instant expirationWarningDate = now.plus(Period.ofDays(7));
+        expiringRepository.deleteByExpirationDateBeforeOrExpirationDateAfter(now, expirationWarningDate);
+        Iterable<License> expiringLicenses = licenseRepository.findAllByExpirationDateAfterAndExpirationDateBefore(now, expirationWarningDate);
+        for (License license : expiringLicenses) {
+            ExpiringLicense expiringEntry = new ExpiringLicense();
+            expiringEntry.setExpirationDate(license.getExpirationDate());
+            expiringEntry.setLicense(license);
+            license.setExpiringEntry(expiringEntry);
+            licenseRepository.save(license);
+        }
     }
 }
